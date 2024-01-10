3d72a062ddbc78c2bcdc8eb1158e5a246563dd3e
Michael Naumov <mnaoumov@gmail.com>
2024-01-09 21:58:12 -0600

    Reformat

--- src/FixRequireModulesPlugin.ts
+++ src/FixRequireModulesPlugin.ts
@@ -1,53 +1,53 @@
 import {
-    Plugin,
+  Plugin,
 } from "obsidian";
 import * as Module from "module";
 
 type TypeofModule = typeof Module;
 
 interface TypeofModuleEx extends TypeofModule {
-    _pathCache: Record<string, string>;
-    _cache: Record<string, Module>;
+  _pathCache: Record<string, string>;
+  _cache: Record<string, Module>;
 }
 
 const builtInModuleNames = [
-    "obsidian",
-    "@codemirror/autocomplete",
-    "@codemirror/collab",
-    "@codemirror/commands",
-    "@codemirror/language",
-    "@codemirror/lint",
-    "@codemirror/search",
-    "@codemirror/state",
-    "@codemirror/text",
-    "@codemirror/view",
-    "@lezer/common",
-    "@lezer/lr",
-    "@lezer/highlight"
+  "obsidian",
+  "@codemirror/autocomplete",
+  "@codemirror/collab",
+  "@codemirror/commands",
+  "@codemirror/language",
+  "@codemirror/lint",
+  "@codemirror/search",
+  "@codemirror/state",
+  "@codemirror/text",
+  "@codemirror/view",
+  "@lezer/common",
+  "@lezer/lr",
+  "@lezer/highlight"
 ];
 
 export default class FixRequireModulesPlugin extends Plugin {
-    async onload(): Promise<void> {
-        const pluginRequire = require;
-        const nodeRequire = window.require;
-        const pathCacheKeySuffix = ["", ...window.module.paths].join("\x00");
-        const ModuleEx = Module.Module as TypeofModuleEx;
+  async onload(): Promise<void> {
+    const pluginRequire = require;
+    const nodeRequire = window.require;
+    const pathCacheKeySuffix = ["", ...window.module.paths].join("\x00");
+    const ModuleEx = Module.Module as TypeofModuleEx;
 
-        for (const builtInModuleName of builtInModuleNames) {
-            const builtInModule = pluginRequire(builtInModuleName);
-            const pathCacheKey = `${builtInModuleName}${pathCacheKeySuffix}`;
-            const fakePath = `${FixRequireModulesPlugin.name}/${builtInModuleName}`;
-            ModuleEx._pathCache[pathCacheKey] = fakePath;
-            const moduleWrapper = new ModuleEx(builtInModuleName);
-            moduleWrapper.loaded = true;
-            moduleWrapper.exports = builtInModule;
-            ModuleEx._cache[fakePath] = moduleWrapper;
+    for (const builtInModuleName of builtInModuleNames) {
+      const builtInModule = pluginRequire(builtInModuleName);
+      const pathCacheKey = `${builtInModuleName}${pathCacheKeySuffix}`;
+      const fakePath = `${FixRequireModulesPlugin.name}/${builtInModuleName}`;
+      ModuleEx._pathCache[pathCacheKey] = fakePath;
+      const moduleWrapper = new ModuleEx(builtInModuleName);
+      moduleWrapper.loaded = true;
+      moduleWrapper.exports = builtInModule;
+      ModuleEx._cache[fakePath] = moduleWrapper;
 
-            this.register(() => {
-                delete ModuleEx._pathCache[pathCacheKey];
-                delete ModuleEx._cache[fakePath];
-                delete nodeRequire.cache[builtInModuleName];
-            });
-        }
+      this.register(() => {
+        delete ModuleEx._pathCache[pathCacheKey];
+        delete ModuleEx._cache[fakePath];
+        delete nodeRequire.cache[builtInModuleName];
+      });
     }
+  }
 }
