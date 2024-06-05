6ef496e001b8f7c94bdae98b03c1bdbfc74f8a36
Michael Naumov <mnaoumov@gmail.com>
2024-06-05 10:41:12 -0600

    Simplify to use Module.require, expose builtInModuleNames

--- src/FixRequireModulesPlugin.ts
+++ src/FixRequireModulesPlugin.ts
@@ -1,55 +1,40 @@
-import {
-  Plugin,
-} from "obsidian";
+import { Plugin } from "obsidian";
 import Module from "module";
 
-type ModuleConstructor = typeof Module;
-
-interface ModuleExConstructor extends ModuleConstructor {
-  _pathCache: Record<string, string>;
-  _cache: Record<string, Module>;
-}
-
-const builtInModuleNames = [
-  "obsidian",
-  "@codemirror/autocomplete",
-  "@codemirror/collab",
-  "@codemirror/commands",
-  "@codemirror/language",
-  "@codemirror/lint",
-  "@codemirror/search",
-  "@codemirror/state",
-  "@codemirror/text",
-  "@codemirror/view",
-  "@lezer/common",
-  "@lezer/lr",
-  "@lezer/highlight"
-];
-
 export default class FixRequireModulesPlugin extends Plugin {
+  public readonly builtInModuleNames = Object.freeze([
+    "obsidian",
+    "@codemirror/autocomplete",
+    "@codemirror/collab",
+    "@codemirror/commands",
+    "@codemirror/language",
+    "@codemirror/lint",
+    "@codemirror/search",
+    "@codemirror/state",
+    "@codemirror/text",
+    "@codemirror/view",
+    "@lezer/common",
+    "@lezer/lr",
+    "@lezer/highlight"
+  ]);
+
   public override onload(): void {
     const pluginRequire = require;
-    const nodeRequire = window.require;
-    const electronRendererModule = window.module;
-    const PATH_CACHE_SEPARATOR = "\x00";
-    const pathCacheKeySuffix = ["", ...electronRendererModule.paths].join(PATH_CACHE_SEPARATOR);
-    const ModuleEx = Module as ModuleExConstructor;
 
-    for (const builtInModuleName of builtInModuleNames) {
-      const builtInModule: unknown = pluginRequire(builtInModuleName);
-      const pathCacheKey = `${builtInModuleName}${pathCacheKeySuffix}`;
-      const fakePath = `${FixRequireModulesPlugin.name}/${builtInModuleName}`;
-      ModuleEx._pathCache[pathCacheKey] = fakePath;
-      const moduleWrapper = new ModuleEx(builtInModuleName);
-      moduleWrapper.loaded = true;
-      moduleWrapper.exports = builtInModule;
-      ModuleEx._cache[fakePath] = moduleWrapper;
+    const originalRequire = Module.prototype.require;
+    const newRequire = ((id: string): any => {
+      if (this.builtInModuleNames.includes(id)) {
+        return pluginRequire(id);
+      }
+      return originalRequire(id);
+    }) as NodeJS.Require;
 
-      this.register(() => {
-        delete ModuleEx._pathCache[pathCacheKey];
-        delete ModuleEx._cache[fakePath];
-        delete nodeRequire.cache[builtInModuleName];
-      });
-    }
+    Object.assign(newRequire, originalRequire);
+
+    Module.prototype.require = newRequire;
+
+    this.register(() => {
+      Module.prototype.require = originalRequire;
+    });
   }
 }
