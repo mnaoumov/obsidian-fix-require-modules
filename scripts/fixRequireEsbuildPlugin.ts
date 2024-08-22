316ce348e4353289bfeef69f7f0961818d39125c
Michael Naumov <mnaoumov@gmail.com>
2024-08-22 17:06:34 -0600

    Extract _requireEsbuild to preserve line numbers

--- scripts/fixRequireEsbuildPlugin.ts
+++ scripts/fixRequireEsbuildPlugin.ts
@@ -8,23 +8,28 @@ import { readNpmPackage } from "obsidian-dev-utils/Npm";
 export function fixRequireEsbuildPlugin(distPath: string): Plugin {
   return {
     name: "fix-require-esbuild",
-    setup(build): void {
-      build.onEnd(async () => {
-        const npmPackage = await readNpmPackage();
+    async setup(build): Promise<void> {
+      const npmPackage = await readNpmPackage();
+      build.initialOptions.banner ??= {};
+      const jsBanner = build.initialOptions.banner["js"] ?? "";
+      build.initialOptions.banner["js"] = `${jsBanner}
 
-        let contents = await readFile(distPath, "utf8");
-        const esbuildReplaceString = `(() => {
-const { join } = require("node:path");
-const { existsSync } = require("node:fs");
-const vault = window.app.vault;
-const esbuildPath = join(vault.adapter.getBasePath(), vault.configDir, "plugins/${npmPackage.name}/node_modules/esbuild/lib/main.js");
-if (existsSync(esbuildPath)) {
-  process.env["ESBUILD_WORKER_THREADS"] = "0";
-  return require(esbuildPath);
-}
-return {};
-})()`;
-        contents = contents.replaceAll("require(\"esbuild\")", esbuildReplaceString);
+var _requireEsbuild = () => {
+  const { join } = require("node:path");
+  const { existsSync } = require("node:fs");
+  const vault = window.app.vault;
+  const esbuildPath = join(vault.adapter.getBasePath(), vault.configDir, "plugins/${npmPackage.name}/node_modules/esbuild/lib/main.js");
+  if (existsSync(esbuildPath)) {
+    process.env["ESBUILD_WORKER_THREADS"] = "0";
+    return require(esbuildPath);
+  }
+  throw new Error("esbuild not found");
+};
+`;
+
+      build.onEnd(async () => {
+        let contents = await readFile(distPath, "utf-8");
+        contents = contents.replaceAll("require(\"esbuild\")", "_requireEsbuild()");
         await writeFile(distPath, contents, "utf-8");
       });
     }
