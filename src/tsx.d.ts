2f56d1578e8ea5f2179b97395ccc2de8b2818e12
Michael Naumov <mnaoumov@gmail.com>
2024-09-09 21:44:42 -0600

    Fix build

--- src/tsx.d.ts
+++ src/tsx.d.ts
@@ -1,6 +1,6 @@
 interface Tsx {
   (): void;
   require: (id: string, fromFile: string | URL) => unknown;
-  resolve: (id: string, fromFile: string | URL, resolveOptions?: { paths?: string[] | undefined } | undefined) => string;
+  resolve: (id: string, fromFile: string | URL, resolveOptions?: { paths?: string[] | undefined }) => string;
   unregister: () => void;
 }
