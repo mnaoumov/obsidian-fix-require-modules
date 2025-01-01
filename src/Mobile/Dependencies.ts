a7b75aa91eddc028683a94c3cdf86b2094b44af3
Michael Naumov <mnaoumov@gmail.com>
2024-12-31 19:05:49 -0600

    Refactor

--- src/Mobile/Dependencies.ts
+++ src/Mobile/Dependencies.ts
@@ -2,10 +2,10 @@ import type { PlatformDependencies } from '../PlatformDependencies.ts';
 
 import { fileSystemWrapper } from './FileSystemWrapper.ts';
 import { requireHandler } from './RequireHandler.ts';
-import { registerScriptDirectoryWatcher } from './ScriptDirectoryWatcher.ts';
+import { scriptDirectoryWatcher } from './ScriptDirectoryWatcher.ts';
 
 export const platformDependencies: PlatformDependencies = {
   fileSystemWrapper,
-  registerScriptDirectoryWatcher,
-  requireHandler
+  requireHandler,
+  scriptDirectoryWatcher
 };
--- src/PlatformDependencies.ts
+++ src/PlatformDependencies.ts
@@ -1,22 +1,18 @@
 import { Platform } from 'obsidian';
 
 import type { FileSystemWrapper } from './FileSystemWrapper.ts';
-import type { FixRequireModulesPlugin } from './FixRequireModulesPlugin.ts';
 import type { RequireHandler } from './RequireHandler.ts';
+import type { ScriptDirectoryWatcher } from './ScriptDirectoryWatcher.ts';
 
 export interface PlatformDependencies {
   fileSystemWrapper: FileSystemWrapper;
-  registerScriptDirectoryWatcher(plugin: FixRequireModulesPlugin, onChange: () => Promise<void>): Promise<void>;
   requireHandler: RequireHandler;
-}
-
-interface PlatformDependenciesModule {
-  platformDependencies: PlatformDependencies;
+  scriptDirectoryWatcher: ScriptDirectoryWatcher;
 }
 
 export async function getPlatformDependencies(): Promise<PlatformDependencies> {
   const module = Platform.isMobile
-    ? await import('./Mobile/Dependencies.ts') as PlatformDependenciesModule
-    : await import('./Desktop/Dependencies.ts') as PlatformDependenciesModule;
+    ? await import('./Mobile/Dependencies.ts')
+    : await import('./Desktop/Dependencies.ts');
   return module.platformDependencies;
 }
