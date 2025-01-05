42d6d34e821c370e67e9a926a818810218eb0e71
Michael Naumov <mnaoumov@gmail.com>
2025-01-05 12:22:01 -0600

    Log last value

--- src/babel/WrapForCodeBlockBabelPlugin.ts
+++ src/babel/WrapForCodeBlockBabelPlugin.ts
@@ -1,13 +1,22 @@
 import type { PluginPass } from '@babel/core';
 import type { Visitor } from '@babel/traverse';
+import type {
+  Expression,
+  Statement
+} from '@babel/types';
 
 import {
   assignmentExpression,
   blockStatement,
+  callExpression,
+
   expressionStatement,
   functionExpression,
   identifier,
-  memberExpression
+  isExpressionStatement,
+  isReturnStatement,
+  memberExpression,
+  nullLiteral
 } from '@babel/types';
 
 import { BabelPluginBase } from './BabelPluginBase.ts';
@@ -21,6 +30,30 @@ export class WrapForCodeBlockBabelPlugin extends BabelPluginBase {
     return {
       Program(path): void {
         const programBody = path.node.body;
+
+        const lastStatement = programBody.pop();
+        let lastStatementExpression = convertToExpression(lastStatement);
+
+        if (!lastStatementExpression) {
+          if (lastStatement) {
+            programBody.push(lastStatement);
+          }
+
+          lastStatementExpression = identifier('undefined');
+        }
+
+        const newLastStatement = expressionStatement(callExpression(
+          memberExpression(
+            identifier('console'),
+            identifier('log')
+          ),
+          [
+            lastStatementExpression
+          ]
+        ));
+
+        programBody.push(newLastStatement);
+
         const wrapperFunction = functionExpression(
           identifier('codeButtonBlockScriptWrapper'),
           [
@@ -44,4 +77,25 @@ export class WrapForCodeBlockBabelPlugin extends BabelPluginBase {
       }
     };
   }
+}
+function convertToExpression(statement: Statement | undefined): Expression | null {
+  if (!statement) {
+    return identifier('undefined');
+  }
+
+  if (isExpressionStatement(statement)) {
+    return statement.expression;
+  }
+
+  if (isReturnStatement(statement)) {
+    if (statement.argument) {
+      return statement.argument;
+    }
+
+    if (statement.argument === null) {
+      return nullLiteral();
+    }
+  }
+
+  return null;
 }
